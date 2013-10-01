de4be652a34a1f0db55ab24a5900bf6d383d673e
Jason Polites <jason.polites@gmail.com>
2013-10-01 16:22:21 -0700

    Moved demo into sdk project

---
+++ demo/src/com/sharethis/loopy/demo/DemoActivity.java
@@ -0,0 +1,120 @@
+package com.sharethis.loopy.demo;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.sharethis.loopy.sdk.Item;
+import com.sharethis.loopy.sdk.Loopy;
+import com.sharethis.loopy.sdk.ShareDialogListener;
+
+/**
+ * Demonstration activity for creating trackable share dialogs and menus.
+ */
+public class DemoActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        // Call onCreate and pass in your API key
+        Loopy.onCreate(this, "<YOUR-API-KEY>");
+
+        // The following code is just for this demo app
+        ArrayAdapter<String> optionsAdapter = new ArrayAdapter<String>(this, R.layout.row, new String[]{
+            "Share Dialog",
+            "Share Options Menu"
+        });
+        ListView list = (ListView) findViewById(R.id.main);
+        list.setAdapter(optionsAdapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+
+                        // Show a share dialog for a single URL
+                        // More detailed data can be provided by sharing an "Item"
+                       showShareDialog("http://www.sharethis.com");
+                       break;
+                    case 1:
+                        startActivity(new Intent(DemoActivity.this, ShareActionProviderActivity.class));
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        Loopy.onDestroy(this);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Loopy.onStart(this);
+    }
+
+    @Override
+    protected void onStop() {
+        Loopy.onStop(this);
+        super.onStop();
+    }
+
+    void showShareDialog(final String urlToShare) {
+
+        // Create an intent.  The URL to be shared will be available in the callback below
+        final Intent dialogIntent = new Intent(Intent.ACTION_SEND);
+        dialogIntent.setType("*/*");
+
+        // Render the share dialog
+        // The listener is used to set the trackable URL on to the share intent.
+        Loopy.showShareDialog(this, "Share to...", urlToShare, dialogIntent, new ShareDialogListener() {
+
+            @Override
+            public void onLinkGenerated(Item item, Throwable error) {
+                if (error == null) {
+                    // We got a tracking shortlink ok, set it in the body of the share (or wherever you like)
+                    dialogIntent.putExtra(Intent.EXTRA_TEXT, item.getShortlink());
+                }
+                else {
+                    // We couldn't get the shortlink, just use the original URL
+                    dialogIntent.putExtra(Intent.EXTRA_TEXT, urlToShare);
+                }
+            }
+
+            // Optional
+            @Override
+            public void onShow(DialogInterface dialog) {
+                // Called when the dialog is shown.
+                Log.i("Loopy", "Share dialog displayed");
+            }
+
+            // Optional
+            @Override
+            public boolean onClick(DialogInterface dialog, ResolveInfo app, Intent shareIntent) {
+                // Called when the user makes a selection
+                Log.i("Loopy", "Share dialog clicked for " + app.activityInfo.name);
+
+                // Return false bey default, or true to handle the click event yourself.
+                return false;
+            }
+
+            // Optional
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                // Called when the user cancels the share dialog
+                Log.i("Loopy", "Share dialog cancelled");
+            }
+        });
+    }
+}
---
+++ demo/src/com/sharethis/loopy/demo/ShareActionProviderActivity.java
@@ -0,0 +1,123 @@
+package com.sharethis.loopy.demo;
+
+//begin-snippet-0
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.sharethis.loopy.sdk.Item;
+import com.sharethis.loopy.sdk.Loopy;
+import com.sharethis.loopy.sdk.ShareCallback;
+
+/**
+ * Demonstrates using trackable share urls in conjuction
+ * with a ShareActionProvider in a standard Android Action Bar
+ */
+public class ShareActionProviderActivity extends ActionBarActivity {
+
+    private ShareActionProvider mShareActionProvider;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Replace the reference to share_menu_activity
+        // with your activity's layout
+        setContentView(R.layout.share_menu_activity);       // <== TODO: Set this
+
+        // Call onCreate and pass in your API key
+        Loopy.onCreate(this, "<YOUR-API-KEY>");             // <== TODO: Set this
+    }
+
+    @Override
+    protected void onDestroy() {
+        Loopy.onDestroy(this);
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Loopy.onStart(this);
+    }
+
+    @Override
+    protected void onStop() {
+        Loopy.onStop(this);
+        super.onStop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+
+        // This would be the url you are wanting to share...
+        final String urlToShare = "http://www.sharethis.com";   // <== TODO: Set this
+
+        // Set the content type for the intent (*/* means ALL)
+        final String contentType = "*/*";                       // <== TODO: Set this
+
+        // The next 3 lines are standard menu inflation commands
+        getMenuInflater().inflate(R.menu.share_menu, menu);
+
+        final MenuItem shareItem = menu.findItem(R.id.action_share);
+
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
+        final Context context = mShareActionProvider.getContext();
+
+        // Ensure we hide the menu while we generate the trackable link
+        shareItem.setVisible(false);
+
+        // Create an intent to share.  The URL to be shared will be available in the callback below
+        final Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+        // Set the conent type for the intent (*/* means ALL)
+        shareIntent.setType(contentType);
+
+        // Create a trackable URL
+        Loopy.shorten(urlToShare, new ShareCallback() {
+
+            @Override
+            public void onResult(final Item item, Throwable error) {
+
+                if (error == null) {
+
+                    // We got a tracking shortlink ok, set it in the body of the share (or wherever you like)
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, item.getShortlink());
+
+                    // Add a listener to record the click
+                    mShareActionProvider.setOnShareTargetSelectedListener(
+
+                            new ShareActionProvider.OnShareTargetSelectedListener() {
+
+                            @Override
+                            public boolean onShareTargetSelected(ShareActionProvider shareActionProvider, Intent intent) {
+                                // Report the share event
+                                reportShare(context, intent);
+
+                                // Return value is ignored, so just return false.
+                                return false;
+                            }
+                        });
+                }
+                else {
+                    // We couldn't get the shortlink, just use the original URL
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, urlToShare);
+                }
+
+                // Set the intent on the share action provider
+                mShareActionProvider.setShareIntent(shareIntent);
+
+                // Finally show (unhide) the menu item
+                shareItem.setVisible(true);
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
+//end-snippet-0
